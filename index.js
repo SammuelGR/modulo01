42d376d5c0c4dae590a89106f06fbc3b22487341
Sammuel Reis <sammugr@hotmail.com>
2020-01-29 22:04:40 -0300

    creates CRUD

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Route params: ?teste=1
 // Query params: /users/1
 // Request body: { user: Sammuel }
 
-const users = ['Sammuel', 'Diego', 'Alface', 'Lucas'];
+const users = ['Sammuel'];
 
 server.get('/users', (req, res) => {
   return res.json(users);
@@ -16,6 +18,31 @@ server.get('/users/:index', (req, res) => {
   const { index } = req.params;
 
   res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
